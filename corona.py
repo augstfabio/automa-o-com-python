6137f098be3db856bb074dd2695de3401fdbbb5c
Rúben Israel <rubenisrael974@gmail.com>
2021-10-08 19:43:25 -0300

    modificando xpath

--- corona.py
+++ corona.py
@@ -6,14 +6,14 @@ def buscar_result():
 
     sleep(3)
 
-    Confirmados = driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]').text
-    Curados = driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]').text
-    Obitos = driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div/div[1]/div[1]/div[1]/div[2]/div[3]/div[1]').text
+    Confir = driver.find_element_by_xpath('//div[1]/div[1]/div[1]/div[2]/div[1]/div[1]').text
+    Curado = driver.find_element_by_xpath('//div[1]/div[1]/div[1]/div[2]/div[2]/div[1]').text
+    Obitos = driver.find_element_by_xpath('//div[1]/div[1]/div[1]/div[2]/div[3]/div[1]').text
 
-    arquivo.write(f'\nCasos confirmados: {Confirmados}\nCasos curados: {Curados}\nCasos com mortes: {Obitos}\n\n')
+    arquivo.write(f'\nCasos confirmados: {Confir}\nCasos curados: {Curado}\nCasos com mortes: {Obitos}\n\n')
 
 opc = Options()
-opc.headless = True # Mostrar navegador > opc.headless = False | Não mostrar navegador > opc.headless = True
+opc.headless = True # Ocultar navegador? True or False
 
 driver = webdriver.Chrome(executable_path='chromedriver.exe', options=opc)
 driver.get('https://www.coronatracker.com/pt-br')
@@ -23,8 +23,8 @@ arquivo = open('casosdocorona.txt', "w", encoding='UTF-8') # Criando arquivo .tx
 arquivo.write('#CASOS DO CORONA NO MUNDO\n')
 buscar_result()
 
-driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div/div[1]/div[1]/div[1]/div[1]/button').click()
-driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div/div[1]/div[1]/div[1]/div[1]/ul/li[4]/a').click()
+driver.find_element_by_xpath('//div/div[1]/div[1]/div[1]/div[1]/button').click()
+driver.find_element_by_xpath('//div/div[1]/div[1]/div[1]/div[1]/ul/li[4]/a').click()
 
 arquivo.write('#CASOS DO CORONA NO BRASIL\n') 
 buscar_result()
